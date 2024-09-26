377c122e488712e8da524e3af9488c26036db399
Divyanshu Singh <112643380+Divyanshudiv@users.noreply.github.com>
2024-09-27 02:08:58 +0530

    v3.

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-AOS.init({
-    easing: 'ease-in-quad',
-  });
+document.addEventListener('DOMContentLoaded', (event) => {
+  const quotes = document.querySelectorAll('#quote-container p');
+  let currentQuote = 0;
+
+  function changeQuote() {
+      quotes[currentQuote].classList.remove('active');
+      currentQuote = (currentQuote + 1) % quotes.length;
+      quotes[currentQuote].classList.add('active');
+  }
+
+  setInterval(changeQuote, 5000); // Change quote every 5 seconds
+});
