3c4c822ff1afa930d811cb1c18d59af92a5b8520
Divyanshu Singh <112643380+Divyanshudiv@users.noreply.github.com>
2024-09-27 12:08:25 +0530

    fixes to news.

--- script.js
+++ script.js
@@ -2,12 +2,11 @@ document.addEventListener('DOMContentLoaded', (event) => {
   const quotes = document.querySelectorAll('#quote-container p');
   let currentQuote = 0;
 
-  function rotateQuotes() {
-    quotes.forEach((quote, index) => {
-        quote.classList.toggle('active', index === currentQuote);
-    });
-    currentQuote = (currentQuote + 1) % quotes.length;
-}
+  function changeQuote() {
+      quotes[currentQuote].classList.remove('active');
+      currentQuote = (currentQuote + 1) % quotes.length;
+      quotes[currentQuote].classList.add('active');
+  }
 
   setInterval(changeQuote, 5000); // Change quote every 5 seconds
 });
